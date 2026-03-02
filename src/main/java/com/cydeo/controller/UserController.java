3f3dc510cb3b129bc210f4c6086197e635bd4673
Umran Ozman <umonal@yahoo.com>
2026-03-02 11:51:45 -0500

    UserController Class updated

--- src/main/java/com/cydeo/controller/UserController.java
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -1,8 +1,14 @@
 package com.cydeo.controller;
 
+import com.cydeo.dto.ResponseWrapper;
+import com.cydeo.dto.UserDTO;
 import com.cydeo.service.UserService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.persistence.Id;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/user")
@@ -11,6 +17,17 @@ public class UserController {
     private final UserService userService;
     public UserController(UserService userService) {
         this.userService = userService;
+    }
+    @GetMapping
+    public ResponseEntity<ResponseWrapper> getUsers(){
+        List<UserDTO> userDTOList = userService.listAllUsers();
+        return ResponseEntity.ok(new ResponseWrapper("Users are successfully retrieves",userDTOList, HttpStatus.OK));
+    }
+
+    @GetMapping("/{username}")
+    public ResponseEntity<ResponseWrapper> getUserByUserName(@PathVariable("username") String userName){
+        UserDTO user = userService.findByUserName(userName);
+        return ResponseEntity.ok(new ResponseWrapper("User is successfully retrieved",user, HttpStatus.OK));
     }
 
 
