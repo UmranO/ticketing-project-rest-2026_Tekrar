fd3e9909b4e9b4c9c35aa4432f1b6a038896847c
Umran Ozman <umonal@yahoo.com>
2026-03-02 18:12:33 -0500

    ProjectController Class updated

--- src/main/java/com/cydeo/controller/ProjectController.java
+++ src/main/java/com/cydeo/controller/ProjectController.java
@@ -1,4 +1,16 @@
 package com.cydeo.controller;
 
+import com.cydeo.service.ProjectService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/project")
 public class ProjectController {
+
+    private final ProjectService projectService;
+
+    public ProjectController(ProjectService projectService) {
+        this.projectService = projectService;
+    }
 }
+
