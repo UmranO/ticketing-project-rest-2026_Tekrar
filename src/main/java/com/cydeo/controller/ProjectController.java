fbbc1bc83526403da401913451ec24b5ef46918b
Umran Ozman <umonal@yahoo.com>
2026-03-02 18:13:35 -0500

    ProjectController Class updated

--- src/main/java/com/cydeo/controller/ProjectController.java
+++ src/main/java/com/cydeo/controller/ProjectController.java
@@ -1,7 +1,13 @@
 package com.cydeo.controller;
 
+import com.cydeo.dto.ProjectDTO;
+import com.cydeo.dto.ResponseWrapper;
 import com.cydeo.service.ProjectService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/project")
@@ -11,6 +17,19 @@ public class ProjectController {
 
     public ProjectController(ProjectService projectService) {
         this.projectService = projectService;
+    }
+
+    @GetMapping
+    public ResponseEntity<ResponseWrapper> getProjects(){
+        List<ProjectDTO> projectDTOList = projectService.listAllProjects();
+        return ResponseEntity.ok(new ResponseWrapper("Projects are successfully retrieved",projectDTOList, HttpStatus.OK));
+    }
+
+    @GetMapping("/{code}")
+    public ResponseEntity<ResponseWrapper> getProjectByCode(@PathVariable("code") String code){
+        ProjectDTO projectDTO = projectService.getByProjectCode(code);
+        return ResponseEntity.ok(new ResponseWrapper("Project is successfully retrieved",projectDTO, HttpStatus.OK));
+
     }
 }
 
