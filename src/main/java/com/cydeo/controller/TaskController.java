d512800bcb2391c6d1bc988cd6e39470c66011b6
Umran Ozman <umonal@yahoo.com>
2026-03-03 11:26:41 -0500

    TaskController Class updated

--- src/main/java/com/cydeo/controller/TaskController.java
+++ src/main/java/com/cydeo/controller/TaskController.java
@@ -1,4 +1,18 @@
 package com.cydeo.controller;
 
+import com.cydeo.service.TaskService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/task")
 public class TaskController {
-}
+
+    private final TaskService taskService;
+
+    public TaskController(TaskService taskService) {
+    this.taskService = taskService; }
+
+
+
+    }
